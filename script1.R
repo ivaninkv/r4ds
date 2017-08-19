a921335897a13b3f930267929e896791b18a5a40
Konstantin <ivaninkv@outlook.com>
2017-08-19 15:40:43 +0300

    exercise 1

--- script1.R
+++ script1.R
@@ -12,3 +12,5 @@ dim(mpg)
 # drv - f = front-wheel drive, r = rear wheel drive, 4 = 4wd
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = hwy, y = cyl))
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = class, y = drv))
