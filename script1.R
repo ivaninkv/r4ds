15f640e3e22c205a26851c005f10f4e30d5844da
Konstantin <ivaninkv@outlook.com>
2017-08-19 15:39:38 +0300

    Initial commit

---
+++ script1.R
@@ -0,0 +1,14 @@
+rm(list = ls())
+gc()
+
+setwd('d:/DS/r4ds/')
+
+library(tidyverse)
+
+# exercise 1 ----
+ggplot(data = mpg)
+dim(mpg)
+?mpg
+# drv - f = front-wheel drive, r = rear wheel drive, 4 = 4wd
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = hwy, y = cyl))
