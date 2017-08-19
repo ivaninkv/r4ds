640472280cb1a47e13310faadcc9d2b8bea637ad
Konstantin <ivaninkv@outlook.com>
2017-08-19 16:12:12 +0300

    exercice 2

--- script1.R
+++ script1.R
@@ -14,3 +14,23 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = hwy, y = cyl))
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = class, y = drv))
+
+
+# exercise 2 ----
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = 'blue')) # incorrect
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy), color = 'blue') # correct
+str(mpg)
+?mpg
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = cty, size = displ))
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = displ))
+?geom_point
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = hwy, y = cty), stroke = 1, shape = 21)
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = hwy, y = cty, color = displ < 5), size = 2)
+
+
