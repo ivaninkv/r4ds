8ee22b6ff5828095f9cab23eeae6f87dc2659541
Konstantin <ivaninkv@outlook.com>
2017-08-19 16:38:01 +0300

    exercise 3

--- script1.R
+++ script1.R
@@ -5,7 +5,7 @@ setwd('d:/DS/r4ds/')
 
 library(tidyverse)
 
-# exercise 1 ----
+# exercise 1 3.2.4 ----
 ggplot(data = mpg)
 dim(mpg)
 ?mpg
@@ -16,7 +16,7 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = class, y = drv))
 
 
-# exercise 2 ----
+# exercise 2 3.3.1 ----
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, color = 'blue')) # incorrect
 ggplot(data = mpg) +
@@ -32,5 +32,36 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = hwy, y = cty), stroke = 1, shape = 21)
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = hwy, y = cty, color = displ < 5), size = 2)
+
+
+# exercise 3.5.1 ---
+# 1
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ displ, nrow = 2)
+# 2
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = drv, y = cyl)) +
+  facet_grid(drv ~ cyl)
+# 3
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ .)
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(. ~ cyl)
+# 4
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ class, nrow = 3)
+
+
+
+
+
+
+
+
+
 
 
