9dfc52cf9d23466d658e84c5f5bfa5f33a4b193a
Konstantin <ivaninkv@outlook.com>
2017-08-20 17:42:31 +0300

    add chapter_5

---
+++ chapter_5.R
@@ -0,0 +1,46 @@
+rm(list = ls())
+gc()
+
+setwd('d:/DS/r4ds/')
+
+library(tidyverse)
+library(nycflights13)
+
+# exercise 1 5.2.4 ----
+# 1
+flights %>% 
+  filter(arr_delay >= 120)
+flights %>% 
+  filter(dest == 'IAH' | dest == 'HOU')
+flights %>% 
+  filter(month %in% 7:9)
+flights %>% 
+  filter(arr_delay >= 120, dep_delay <= 0)
+flights %>% 
+  filter(dep_delay >= 60, air_time >= 30)
+flights %>% 
+  filter(dep_time >= 0 & dep_time <= 600)
+# 2
+?between
+flights %>% 
+  filter(between(dep_time, 0, 600))
+# 3
+View(flights %>% 
+  filter(is.na(dep_time)))
+# 4
+NA ^ 0
+NA | TRUE
+FALSE & NA
+
+
+# exercise 2 5.3.1 ----
+flights %>% 
+  arrange(desc(is.na(dep_time)))
+flights %>% 
+  arrange(desc(arr_delay))
+flights %>% 
+  arrange(air_time)
+flights %>% 
+  arrange(desc(air_time))
+
+
